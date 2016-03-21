8a40cb4add8c7607dc41fa7c467f1b4961a9d9c0
lpital <zhouzhe_zhe@126.com>
2016-03-22 00:11:06 +0800

    new

--- main.go
+++ main.go
@@ -6,11 +6,46 @@ import (
 
 	"github.com/hanwen/go-fuse/fuse/nodefs"
 	"github.com/hanwen/go-fuse/fuse/pathfs"
+	"github.com/lpital/exfs"
+	// "github.com/lpital/exfs/lpital"
 )
 
 //Exfs is a implementation of pathfs.FileSystem
 type ExFs struct {
 	pathfs.FileSystem
+}
+
+func (me *ExFs) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.Status) {
+	switch name {
+	case "NewFile":
+		return &fuse.Attr{
+			Mode: fuse.S_IFREG | 0644, Size: uint64(len(name)),
+		}, fuse.OK
+	case "":
+		return &fuse.Attr{
+			Mode: fuse.S_IFDIR | 0755,
+		}, fuse.OK
+	}
+	return nil, fuse.ENOENT
+}
+
+func (me *HelloFs) OpenDir(name string, context *fuse.Context) (c []fuse.DirEntry, code fuse.Status) {
+	if name == "" {
+		c = []fuse.DirEntry{{Name: "NewFile", Mode: fuse.S_IFREG}}
+		return c, fuse.OK
+	}
+	return nil, fuse.ENOENT
+}
+
+func (me *ExFs) Open(name string, flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
+
+	if name != "NewFile" {
+		return nil, fuse.ENOENT
+	}
+	if flags&fuse.O_ANYWRITE != 0 {
+		return nil, fuse.EPERM
+	}
+	return nodefs.NewDataFile([]byte("This is a new File in exfs.\n")), fuse.OK
 }
 
 // A exfs will be mount at the path in the first arg
